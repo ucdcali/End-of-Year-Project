682ba351a6a3e3dd0383b2bdb05603d3974f82e9
Willa-EB <WE28320@westridge.org>
2026-04-30 10:50:31 -0700

    updated controller

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,20 +1,66 @@
-import mongoose from 'mongoose';
+import { Meal } from '../models/Meal.js'
+//import { Menu } from '../models/Menu.js'
 
-import express from 'express';
-import { Meal } from '../models/Meal.js';
-
-export const router = express.Router();
-
-
-router.get('/', async (req, res, next) => {
+export const homePage = async (req, res, next) => {
     try {
-        const meals = await Meal.find();
-
-        res.render('index', {
-            meal
-        })
+      res.redirect("/admin")
+        // const meals = await Meal.find()
+        // if (date = meals.date) {
+        //     res.json(meals[1])
+        // }
+        // res.render('index', {
+        //   title: "Commons App",
+        //   meals
+        // })
     }
     catch (err) {
         next(err)
     }
-})
+}
+
+export const adminPage = async (req, res, next) => {
+  try{
+  const meals = await Meal.find()
+    if (date = meals.date) {
+    res.json(meals[1])
+    }
+    res.render('index', {
+    title: "Commons App",
+    meals
+    })
+  }
+    catch (err) {
+        next(err)
+    }
+}
+
+
+export const createMeal = async (req, res, next) => {
+    try {
+      const { title, diet, img } = req.body;
+      await Meal.create({ title, diet, img });     
+      res.redirect("/admin")
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+export const deleteMeal = async(req, res, next) => {
+  try {
+    const meal = await Meal.findByIdAndDelete(req.params.id);
+    res.redirect('/admin')
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const editMeal = async(req, res, next) => {
+  try{
+    const meal = await Meal.findById(req.params.id);
+    res.render("edit", { meal });
+    }
+  catch (err) {
+    next(err)
+  }
+}
