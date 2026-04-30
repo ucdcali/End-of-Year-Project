d8ca082cbeabd2a90a7bb86ec9a0b5f1781fc2d1
fpejic <167799897+fpejic@users.noreply.github.com>
2026-04-30 10:59:57 -0700

    updated ejs
    :D

--- routes/routes.js
+++ routes/routes.js
@@ -1,30 +1,26 @@
-<<<<<<< Updated upstream
-=======
-import express from 'express';
-import * as mealController from '../controllers/messageController.js';
-import { Meal } from '../models/Meal.js'
+import express from "express";
+import * as mealController from "../controllers/messageController.js";
+import { Meal } from "../models/Meal.js";
 export const router = express.Router();
 //import { Menu } from '../models/Menu.js'
 
 //router.get('/', async (req, res, next) => {
-    try {
-        const meals = await Meal.find()
-        //console.log(meals)
-        res.json(meals[1])
-    }
-    catch (err) {
-        next(err)
-    }
+try {
+  const meals = await Meal.find();
+  //console.log(meals)
+  res.json(meals[1]);
+} catch (err) {
+  next(err);
+}
 //})
 
 //homepage
-router.get('/', mealController.homePage);
+router.get("/", mealController.homePage);
 //adminpage
-router.get('/admin', mealController.adminPage)
+router.get("/admin", mealController.adminPage);
 //create a meal
-router.post('/admin/create', mealController.createMeal);
+router.post("/admin/create", mealController.createMeal);
 //edit a meal
-router.post('/admin/edit/:id', mealController.editMeal);
+router.post("/admin/edit/:id", mealController.editMeal);
 //delet a meal
-router.post('/admin/delete/:id', mealController.deleteMeal);
->>>>>>> Stashed changes
+router.post("/admin/delete/:id", mealController.deleteMeal);
