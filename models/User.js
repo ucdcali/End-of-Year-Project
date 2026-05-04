e138ad7cc098d7ec61372fb6fd9541e767547059
Willa-EB <WE28320@westridge.org>
2026-05-04 10:52:59 -0700

    login and new controller

--- models/User.js
+++ models/User.js
@@ -1,7 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: String
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 60
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      unique: true,
+      maxlength: 120
+    },
+    passwordHash: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model('User', userSchema);
